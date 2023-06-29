f9fd507131c7cb37635458844e1d8cb44bac11c9
Adomas <107749872+0xleft@users.noreply.github.com>
2023-06-29 08:53:56 +0200

    add stuff

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ppgl',
+    version='1.0',
+    description='A tkinter library that juts works',
+    author='0xleft',
+    author_email='your@email.com',
+    url='https://github.com/0xleft/ppgl',
+    packages=['ppgl'],
+)
