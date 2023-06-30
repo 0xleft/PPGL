38ce28a30841a01db8035d4956613833b9f50941
AdomasDauda <107749872+0xleft@users.noreply.github.com>
2023-06-30 20:07:45 +0100

    version increase

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ppgl',
-    version='1.0',
+    version='1.0.1',
     description='A tkinter library that juts works',
     author='0xleft',
     author_email='test@test.com',
